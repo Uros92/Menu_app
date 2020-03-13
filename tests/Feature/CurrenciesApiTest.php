a42aaf1cfb4f6afb20ed9956d79d6978b86f6d3d
Uros <stosicuros12345@gmail.com>
2020-03-13 17:48:37 +0100

    - updated test method name

--- tests/Feature/CurrenciesApiTest.php
+++ tests/Feature/CurrenciesApiTest.php
@@ -11,11 +11,11 @@ use Symfony\Component\HttpFoundation\Response;
 class CurrenciesApiTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * Test external api call for currencies exchange rates
      *
      * @return void
      */
-    public function testExample()
+    public function testExternalApiForCurrenciesExchangeRates()
     {
         $client = new Client();
         $response = $client->get('http://api.currencylayer.com/live?access_key=0eea08dbc3e1b49e52597bcd2d86b61b&currencies=JPY,GBP,EUR');
